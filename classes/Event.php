25139810f8f46dfb40182a4dedd30b82bdc75931
jamiemac262 <jamiemac262@hotmail.co.uk>
2016-12-11 11:13:26 +0000

    Added functions for Events
    Called those methods from main.php

--- classes/Event.php
+++ classes/Event.php
@@ -26,6 +26,91 @@ class Event{
 		
 	}
 	
+	function getAll(){
+		
+		$conn = self::connect();//connect to the database
+		$sth = $conn->prepare("SELECT * FROM event where resolved = 0");
+		$sth->execute();
+		$result = $sth->setFetchMode(PDO::FETCH_ASSOC); 
+		$events = $sth->fetchAll();
+		
+		return $events;
+	}
+	
+	function get(){
+		
+		$conn = self::connect();//connect to the database
+		$sth = $conn->prepare("SELECT * FROM event where id = :id");
+		$sth->bindParam(':id', $id, PDO::PARAM_INT);
+		$sth->execute();
+		$result = $sth->setFetchMode(PDO::FETCH_ASSOC; 
+		$events = $sth->fetchAll();
+		
+		return $events;
+	}
+	
+	function getResolved(){
+		$conn = self::connect();//connect to the database
+		$sth = $conn->prepare("SELECT * FROM event where resolved = 1");
+		$sth->execute();
+		$result = $sth->setFetchMode(PDO::FETCH_ASSOC); 
+		$events = $sth->fetchAll();
+		
+		return events;
+	}
+	
+	function create($title, $message, $status){
+		
+		$date = new DateTime();
+		$f_date = $date->format('Y-m-d H:i:s');
+		
+		$conn = self::connect();
+		$sth = $conn->prepare("INSERT INTO event (title, message, date, status, resolved) VALUES (:title, :message, :date, :status, 0)");
+		$sth->bindParam(':title', $title, PDO::PARAM_STR);
+		$sth->bindParam(':message', $message, PDO::PARAM_STR);
+		$sth->bindParam(':date', $f_date, PDO::PARAM_STR);
+		$sth->bindParam(':status', $status, PDO::PARAM_INT);
+		$sth->execute();
+		
+	}
+	
+	function editEvent($id, $title, $message, $status){
+		
+		$conn = self::connect();
+		$sth = $conn->prepare("update event SET title=:title, message=:message, status=:status WHERE id=:id");
+		$sth->bindParam(':id', $id, PDO::PARAM_INT);
+		$sth->bindParam(':title', $title, PDO::PARAM_STR);
+		$sth->bindParam(':message', $message, PDO::PARAM_STR);
+		$sth->bindParam(':status', $status, PDO::PARAM_INT);
+		$sth->execute();	
+		
+	}
+	
+	function deleteEvent($id){
+		$conn = self::connect();//connect to the database
+		$sth = $conn->prepare("DELETE FROM event where id = :id");
+		$sth->bindParam(':id', $id, PDO::PARAM_INT);
+		$sth->execute();
+		
+		
+	}
+	
+	function updateStatus($id, $status){
+		
+		$conn = self::connect();
+		$sth = $conn->prepare("update event SET status=:status WHERE id=:id");
+		$sth->bindParam(':id', $id, PDO::PARAM_INT);
+		$sth->bindParam(':status', $status, PDO::PARAM_INT);
+		$sth->execute();	
+	}
+	
+	function resolve($id){
+		$conn = self::connect();
+		$sth = $conn->prepare("update event SET resolved=1 WHERE id=:id");
+		$sth->bindParam(':id', $id, PDO::PARAM_INT);
+		$sth->execute();
+	}
+	
 }
 
 ?>
--- classes/main.php
+++ classes/main.php
@@ -19,5 +19,29 @@ if($fn == "login"){
 	echo $user->doLogin($_GET["name"], $_GET["pass"]);
 	
 }
+//Event functions
+elseif($fn == "allEvent"){
+	echo toJson($event->getAll());
+}elseif($fn == "getEvent"){
+	echo toJson($event->get($_GET[$id]));
+}elseif($fn == "getResolvedEvent"){
+	echo toJson($event->getResolved($_GET[$id]));
+}elseif($fn == "newEvent"){
+	echo toJson($event->create($_GET["title"], $_GET["message"], $_GET["status"]));
+}elseif($fn == "updateEvent"){
+	echo toJson($event->updateEvent($_GET["title"], $_GET["message"], $_GET["status"]));
+}elseif($fn == "delEvent"){
+	echo toJson($event->deleteEvent($_GET["id"]));
+}elseif($fn == "updateStatus"){
+	echo toJson($event->updateStatus($_GET["id"], $_GET["status"]));
+}elseif($fn == "resolve"){
+	echo toJson($event->resolve($_GET["id"]));
+}
 
+
+function toJson($val){
+	
+	return json_encode($val);
+	
+}
 ?>
