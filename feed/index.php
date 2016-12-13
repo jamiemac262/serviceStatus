c84a1b91a6a8a5e5558e08b90c06b3de0caec636
jamiemac262 <jamiemac262@hotmail.co.uk>
2016-12-13 20:54:51 +0000

    Fixed a bug that stopped the code completing

--- feed/index.php
+++ feed/index.php
@@ -1,9 +1,10 @@
 <?php
+
     header("Content-Type: application/rss+xml; charset=ISO-8859-1"); 
     $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
     $rssfeed .= '<rss version="2.0">';
     $rssfeed .= '<channel>';
- 
+
 		$servername = "localhost";
 		$username = "makeitfo_challenge";
 		$password = "7TIOI7B[wG(#";
@@ -12,7 +13,6 @@
 			$conn = new PDO("mysql:host=$servername;dbname=makeitfo_challenge", $username, $password);
 			// set the PDO error mode to exception
 			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			return $conn; 
 		}
 		catch(PDOException $e){
 			echo "Connection failed: " . $e->getMessage();
